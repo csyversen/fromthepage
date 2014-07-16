b6858e25b3072e854a7befb8ff6498e805760715
Chris Syversen <csyversen@gmail.com>
2014-07-16 15:05:28 -0500

    updated keep_releases to 10

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :linked_files, %w{config/database.yml}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-set :keep_releases, 5
+set :keep_releases, 10
 
 namespace :deploy do
 
